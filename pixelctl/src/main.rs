f3bf05eab7bc469ce4e69b3e8c6d3cd0f661167d
Michael Williams <michael.williams@enspiral.com>
2018-02-09 23:44:23 +1300

    all the threads!

--- pixelctl/src/main.rs
+++ pixelctl/src/main.rs
@@ -3,16 +3,18 @@ extern crate kiss3d;
 extern crate nalgebra as na;
 
 use std::thread;
-use std::sync::mpsc::{channel, Receiver};
+use std::sync::mpsc::{channel, Sender, Receiver, Iter};
 use na::{Vector3, UnitQuaternion, Translation3};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
 
+type Time = f32;
+
 struct ClockMessage {
-    pub time: f32
+    pub time: Time
 }
 
-fn create_clock(fps: u32) -> Receiver<ClockMessage> {
+fn create_clock_rx(fps: u32) -> Receiver<ClockMessage> {
     let (clock_tx, clock_rx) = channel();
     thread::spawn(move|| {
         let mut fps = fps_clock::FpsClock::new(fps);
@@ -28,27 +30,47 @@ fn create_clock(fps: u32) -> Receiver<ClockMessage> {
     return clock_rx;
 }
 
-fn main() {
-    let clock = create_clock(60);
+struct RenderMessage {
+    pub time: Time
+}
 
-    let mut window = Window::new("Kiss3d: points");
+fn create_render_tx() -> Sender<RenderMessage> {
+    let (render_tx, render_rx) = channel();
+    thread::spawn(move|| {
+        let mut window = Window::new("Kiss3d: points");
 
-    let mut a = window.add_cube(1.0, 1.0, 1.0);
-    a.set_color(1.0, 0.0, 0.0);
+        let mut a = window.add_cube(1.0, 1.0, 1.0);
+        a.set_color(1.0, 0.0, 0.0);
 
-    window.set_light(Light::StickToCamera);
-    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+        window.set_light(Light::StickToCamera);
+        let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
 
-    for clock in clock.iter() {
-        println!("time: {}", clock.time);
+        let render_iter: Iter<RenderMessage> = render_rx.iter();
+        for render_message in render_iter {
+            println!("time: {}", render_message.time);
 
-        a.prepend_to_local_rotation(&rot);
-        let translation = Translation3::new(clock.time / 1.0e9, 0.0, 0.0);
-        a.set_local_translation(translation);
+            a.prepend_to_local_rotation(&rot);
+            let translation = Translation3::new(render_message.time / 1.0e9, 0.0, 0.0);
+            a.set_local_translation(translation);
 
-        if !window.render() {
-          return;
+            if !window.render() {
+              return;
+            }
         }
+    });
+    return render_tx;
+}
+
+fn main() {
+    let clock_rx = create_clock_rx(60);
+    let render_tx = create_render_tx();
+
+    let clock_iter: Iter<ClockMessage> = clock_rx.iter();
+    for clock_message in clock_iter {
+        let render_message = RenderMessage {
+            time: clock_message.time
+        };
+        render_tx.send(render_message).unwrap();
     }
 }
 
@@ -67,8 +89,8 @@ struct ScenePixel {
 }
 
 struct PixelCtl {
-    pub scenePixels: Vec<ScenePixel>,
-    pub ledPixels: Vec<LedPixel>
+    pub scene_pixels: Vec<ScenePixel>,
+    pub led_pixels: Vec<LedPixel>
 }
 
 struct LedPixel {

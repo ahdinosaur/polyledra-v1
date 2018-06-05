be9664cf6053668acb5310a7ce75fd63680ee625
Michael Williams <michael.williams@enspiral.com>
2018-06-06 01:34:22 +1200

    leftover util

--- controller-app/src/util/mod.rs
+++ controller-app/src/util/mod.rs
@@ -1,1 +1,14 @@
 pub mod compute_edge_lengths;
+
+fn mapf (x: f32, in_min: f32, in_max: f32, out_min: f32, out_max: f32) -> f32 {
+  (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+}
+
+fn mapl (x: f32, in_min: f32, in_max: f32, out_min: f32, out_max: f32) -> f32 {
+  let min = out_min.log(2.);
+  let max = out_max.log(2.);
+
+  let scale = (max - min) / (in_max - in_min);
+
+  (min + scale * (x - in_min)).exp()
+}

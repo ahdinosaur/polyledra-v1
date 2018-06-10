8dd9208c643c18956409b34aa91ea7afe4ee2d5a
Michael Williams <michael.williams@enspiral.com>
2018-06-10 19:34:48 +1200

    make it easy to have separate playground and bone configs

--- controller-app/src/main.rs
+++ controller-app/src/main.rs
@@ -10,6 +10,7 @@ extern crate noise;
 #[macro_use] extern crate structopt;
 extern crate rand;
 
+use std::env;
 use structopt::StructOpt;
 use clap_verbosity_flag::Verbosity;
 
@@ -33,16 +34,21 @@ fn main() {
 
     cli_options.verbose.setup_env_logger("controller-app").unwrap();
 
-    let abstract_shape = shape::Tetrahedron::new(1.0);
+    let edge_length = env::var("EDGE_LENGTH").unwrap().parse::<f32>().unwrap();
+    let pixel_density = env::var("PIXEL_DENSITY").unwrap().parse::<f32>().unwrap();
+    let num_arms = env::var("NUM_ARMS").unwrap().parse::<usize>().unwrap();
+    let fps = env::var("FPS").unwrap().parse::<u32>().unwrap();
+
+    let abstract_shape = shape::Tetrahedron::new(edge_length);
     let shape = shape::Shape::new(shape::ShapeOptions {
         abstract_shape,
-        pixel_density: 30.0,
-        num_arms: 3
+        pixel_density,
+        num_arms
     });
 
     let (control_tx, control_rx) = control::create_control_channel();
 
-    control::connect_clock(60, control_tx.clone());
+    control::connect_clock(fps, control_tx.clone());
 
     let display_tx = display::create_display_tx(control_tx.clone());
     let render_tx = render::create_render_tx(display_tx);
